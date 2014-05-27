00248cfbbada857a29f59b2ec51fd97ca88c9148
Ryan Yan <rawbenny@gmail.com>
2014-05-27 12:38:06 -0700

    rename RunListActivity to ListActivity

--- Gradle Library Projects/library/src/com/pwc/sample/lib1/activity/ListActivity.java
+++ Gradle Library Projects/library/src/com/pwc/sample/lib1/activity/ListActivity.java
@@ -1,11 +1,14 @@
 package com.pwc.sample.lib1.activity;
 
+import android.support.v4.app.Fragment;
+
 import com.pwc.sample.activity.SingleFragmentActivity;
 import com.pwc.sample.lib1.activity.fragment.RunListFragment;
 
-import android.support.v4.app.Fragment;
-
-public class RunListActivity extends SingleFragmentActivity {
+/**
+ * Created by ryan on 5/27/14.
+ */
+public class ListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
